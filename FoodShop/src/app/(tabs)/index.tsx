5c65914b79f298da6c75ae878ba56e05ce1d3e6f
njeveh <njeveh@gmail.com>
2024-06-17 23:10:52 +0300

    add product list page

--- FoodShop/src/app/(tabs)/index.tsx
+++ FoodShop/src/app/(tabs)/index.tsx
@@ -1,17 +1,23 @@
 import { StyleSheet} from 'react-native';
 
-import { View } from '@/src/components/Themed';
-import Colors from '@/src/constants/Colors';
-import products from '@/assets/data/products';
-import ProductListItem from '@/src/components/ProductListItem';
+import { View, } from '@components/Themed';
+import { FlatList} from 'react-native';
+import Colors from '@/constants/Colors';
+import products from '@assets/data/products';
+import ProductListItem from '@components/ProductListItem';
 
 const product = products[0];
 
 export default function MenuScreen() {
   return (
     <View>
-      <ProductListItem product={products[0]}/>
-      <ProductListItem product={products[1]}/>
+      <FlatList
+      data={products}
+      renderItem={({item}) => <ProductListItem product={item}/>}
+      numColumns={2}
+      contentContainerStyle={{ gap: 10, padding: 10 }}
+      columnWrapperStyle={{ gap: 10 }}
+      />
     </View>
   );
 }
--- FoodShop/src/components/ProductListItem.tsx
+++ FoodShop/src/components/ProductListItem.tsx
@@ -1,14 +1,21 @@
 import { StyleSheet, Image } from 'react-native';
-import { Text, View } from '@/src/components/Themed';
-import Colors from '@/src/constants/Colors';
+import { Text, View } from '@components/Themed';
+import Colors from '@/constants/Colors';
+import { Product } from '../types';
 
-const ProductListItem = ({product}) => {
+const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
+
+type ProductListItemProps = {
+    product: Product;
+}
+const ProductListItem = ({product}: ProductListItemProps) => {
     return(
       <>
         <View style={styles.container}>
           <Image
-          source={{ uri: product.image }}
+          source={{ uri: product.image || defaultPizzaImage}}
           style={styles.image}
+          resizeMode='contain'
           />
           <Text style={styles.title}>{product.name}</Text>
           <Text style={styles.price}>${product.price}</Text>
@@ -25,6 +32,8 @@ const ProductListItem = ({product}) => {
       backgroundColor: 'white',
       padding: 10,
       borderRadius: 20,
+      flex: 1,
+      maxWidth: '50%'
   
     },
     image: {
